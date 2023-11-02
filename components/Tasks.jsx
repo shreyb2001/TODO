1fa540815b1ff34d700fd7d24c78fea40c735193
shrey bhardwaj <21uec125@gmail.com>
2023-11-02 22:21:54 +0530

    final

--- components/Tasks.jsx
+++ components/Tasks.jsx
@@ -1,72 +1,6 @@
 import React from "react";
 import Task from "./Task";
 
-const fakeTodo = [
-  {
-    title: "Work out",
-    type: "work",
-    completed: false,
-    owner: "21uec125@lnmiit.ac.in",
-    __v: 0,
-  },
-  {
-    title: "Halo Frinds ",
-    type: "work",
-    completed: false,
-    owner: "bloodconnect.notion@gmail.com",
-    __v: 0,
-  },
-  {
-    title: "fuck the person sitting near me",
-    type: "work",
-    completed: false,
-    owner: "khushalgoyal77@gmail.com",
-    __v: 0,
-  },
-  {
-    title: "Ahhhhh fuck on the bed",
-    type: "home",
-    completed: false,
-    owner: "khushalgoyal77@gmail.com",
-    __v: 0,
-  },
-  {
-    title: "vdgsdzfgvxcvzxc",
-    type: "work",
-    completed: false,
-    owner: "khushalgoyal77@gmail.com",
-    __v: 0,
-  },
-  {
-    title: "zxcvzxcvzxcv",
-    type: "work",
-    completed: false,
-    owner: "khushalgoyal77@gmail.com",
-    __v: 0,
-  },
-  {
-    title: "zxcvzxcvzxcv",
-    type: "work",
-    completed: false,
-    owner: "khushalgoyal77@gmail.com",
-    __v: 0,
-  },
-  {
-    title: "zxcvzxcvcvzxcv",
-    type: "work",
-    completed: false,
-    owner: "khushalgoyal77@gmail.com",
-    __v: 0,
-  },
-  {
-    title: "zcxvzxcvzxcv",
-    type: "work",
-    completed: false,
-    owner: "khushalgoyal77@gmail.com",
-    __v: 0,
-  },
-];
-
 const Tasks = ({ tasks }) => {
   return (
     <div className="px-4 max-h-[22rem] flex flex-col gap-5 overflow-y-scroll">
@@ -75,7 +9,7 @@ const Tasks = ({ tasks }) => {
           <p>No Tasks Available</p>
         </div>
       )}
-      {fakeTodo.map((task, index) => (
+      {tasks.map((task, index) => (
         <Task key={index} todo={task} />
       ))}
     </div>
