5db157ca9710c7748c024c49519070a79bd95d46
shrey bhardwaj <21uec125@gmail.com>
2023-11-02 22:34:34 +0530

    final

--- components/Tasks.jsx
+++ components/Tasks.jsx
@@ -3,7 +3,7 @@ import Task from "./Task";
 
 const Tasks = ({ tasks }) => {
   return (
-    <div className="px-4 max-h-[22rem] flex flex-col gap-5 overflow-y-scroll">
+    <div className="px-4 max-h-[22rem] no-scrollbar flex flex-col gap-5 overflow-y-scroll">
       {tasks.length === 0 && (
         <div className="bg-white flex justify-center items-center p-4 rounded-2xl font-bold text-[#717082b4]">
           <p>No Tasks Available</p>
